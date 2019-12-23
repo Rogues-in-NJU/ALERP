aac5f0ba62543e2534819141986d6f435eec50c5
xujiangcheng <mf1832197@smail.nju.edu.cn>
2019-12-23 14:17:18 +0800

    add auth page

--- alerp-electron/src/app/pages/workspace/user-management/user-management-info/user-management-info.component.ts
+++ alerp-electron/src/app/pages/workspace/user-management/user-management-info/user-management-info.component.ts
@@ -7,6 +7,8 @@ import {UserManagementService} from "../../../../core/services/user-management.s
 import {ResultVO} from "../../../../core/model/result-vm";
 import {HttpErrorResponse} from "@angular/common/http";
 import {NzMessageService} from "ng-zorro-antd";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {debounceTime} from "rxjs/internal/operators";
 
 @Component({
   selector: 'user-management-info',
@@ -15,19 +17,28 @@ import {NzMessageService} from "ng-zorro-antd";
 })
 export class UserManagementInfoComponent implements ClosableTab, OnInit {
 
+  userManagementForm: FormGroup;
   isLoading: boolean = true;
   userManagementId: string;
   userManagementData: UserManagementInfoVO;
+  isSaving: boolean = false;
 
   constructor(private closeTabService: TabService,
               private route: ActivatedRoute,
               private router: Router,
+              private fb: FormBuilder,
               private userManagement: UserManagementService,
               private message: NzMessageService) {
 
   }
 
   ngOnInit(): void {
+    this.userManagementForm = this.fb.group({
+      name: [null],
+      phone_number: [null],
+      radioValue1: [0, Validators.required],
+      radioValue2: [null, Validators.required]
+    });
     this.userManagementId = this.route.snapshot.params['id'];
     this.userManagement.find(this.userManagementId)
       .subscribe((res: ResultVO<UserManagementInfoVO>) => {
@@ -36,6 +47,31 @@ export class UserManagementInfoComponent implements ClosableTab, OnInit {
         }
         this.isLoading = false;
         this.userManagementData = res.data;
+      }, (error: HttpErrorResponse) => {
+        this.message.error(error.message);
+      });
+  }
+
+  saveUser(): void {
+    if (!this.userManagementForm.valid) {
+      return;
+    }
+    let formData: any = this.userManagementForm.getRawValue();
+    let userManagementAdd: UserManagementInfoVO = {
+      name: formData.name,
+      phone_number: formData.phone_number,
+      // radioValue1: formData.radioValue1,
+      // radioValue2: formData.radioValue2
+    };
+
+    this.isSaving = true;
+    this.userManagement.save(userManagementAdd)
+      .pipe(debounceTime(3000))
+      .subscribe((res: ResultVO<any>) => {
+        console.log(res);
+        this.message.success('添加成功!');
+        this.isSaving = false;
+        // TODO: 跳转回列表页面
       }, (error: HttpErrorResponse) => {
         this.message.error(error.message);
       });
