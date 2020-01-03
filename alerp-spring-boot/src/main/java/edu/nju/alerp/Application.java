03eb8eae6ad7d40116b79e197504c74a878f3879
yqe <541123989@qq.com>
2020-01-03 13:34:55 +0800

    feat：跨域请求

--- alerp-spring-boot/src/main/java/edu/nju/alerp/Application.java
+++ alerp-spring-boot/src/main/java/edu/nju/alerp/Application.java
@@ -2,10 +2,12 @@ package edu.nju.alerp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 public class Application {
 
+    @CrossOrigin(origins = {"http://localhost:8080", "null"}, allowCredentials = "true", allowedHeaders = "", methods = {})
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
