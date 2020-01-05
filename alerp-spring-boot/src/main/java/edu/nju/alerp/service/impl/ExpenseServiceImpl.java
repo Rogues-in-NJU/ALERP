1ac241a94c7bdd3000081d0c137ea12c8729f72a
龙宝 <luhailong.lhl@alibaba-inc.com>
2020-01-05 20:28:17 +0800

    自测，修复部分bug

--- alerp-spring-boot/src/main/java/edu/nju/alerp/service/impl/ExpenseServiceImpl.java
+++ alerp-spring-boot/src/main/java/edu/nju/alerp/service/impl/ExpenseServiceImpl.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import javax.servlet.http.HttpSession;
 
+import edu.nju.alerp.common.DocumentsIdFactory;
 import edu.nju.alerp.common.ListResponse;
 import edu.nju.alerp.common.NJUException;
 import edu.nju.alerp.dto.ExpenseDTO;
 import edu.nju.alerp.entity.Expense;
+import edu.nju.alerp.enums.CityEnum;
+import edu.nju.alerp.enums.DocumentsType;
 import edu.nju.alerp.enums.ExceptionEnum;
 import edu.nju.alerp.repo.ExpenseRepository;
 import edu.nju.alerp.service.ExpenseService;
@@ -28,9 +31,14 @@ public class ExpenseServiceImpl implements ExpenseService {
     @Autowired
     private ExpenseRepository expenseRepository;
 
+    @Autowired
+    private DocumentsIdFactory documentsIdFactory;
+
     @Override
     public int addExpense(ExpenseDTO expenseDTO) {
-        Expense expense = Expense.builder().createdAt(DateUtils.getToday()).createdBy(getUserId()).build();
+        Expense expense = Expense.builder().createdAt(DateUtils.getToday()).createdBy(CommonUtils.getUserId()).
+            code(documentsIdFactory.generateNextCode(DocumentsType.ARREAR_ORDER, CityEnum.of(CommonUtils.getCity())))
+            .build();
         BeanUtils.copyProperties(expenseDTO, expense);
         Expense result = expenseRepository.save(expense);
         return result.getId();
@@ -43,7 +51,7 @@ public class ExpenseServiceImpl implements ExpenseService {
             throw new NJUException(ExceptionEnum.ILLEGAL_REQUEST, "未查到该公司支出");
         }
         expense.setDeletedAt(DateUtils.getToday());
-        expense.setDeletedBy(getUserId());
+        expense.setDeletedBy(CommonUtils.getUserId());
         Expense result = expenseRepository.save(expense);
         return result.getId();
     }
@@ -59,8 +67,4 @@ public class ExpenseServiceImpl implements ExpenseService {
         return ListResponseUtils.getListResponse(expenseList, pageIndex, pageSize);
     }
 
-    private int getUserId() {
-        HttpSession session = CommonUtils.getHttpSession();
-        return session.getAttribute("userId") == null ? 0 : (int)session.getAttribute("userId");
-    }
 }
