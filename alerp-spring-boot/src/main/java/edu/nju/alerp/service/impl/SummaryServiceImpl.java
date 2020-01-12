48e9833657549be1a3e3ddf481530424c742e9fc
龙宝 <luhailong.lhl@alibaba-inc.com>
2020-01-12 21:46:09 +0800

    汇总信息模块的controller和VO

---
+++ alerp-spring-boot/src/main/java/edu/nju/alerp/service/impl/SummaryServiceImpl.java
@@ -0,0 +1,23 @@
+package edu.nju.alerp.service.impl;
+
+import edu.nju.alerp.service.SummaryService;
+import edu.nju.alerp.vo.SummaryInfoVO;
+import edu.nju.alerp.vo.SummaryProductInfoVO;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author luhailong
+ * @date 2020/01/12
+ */
+@Service
+public class SummaryServiceImpl implements SummaryService {
+    @Override
+    public SummaryInfoVO getSummaryInfo() {
+        return null;
+    }
+
+    @Override
+    public SummaryProductInfoVO getSummaryProductInfo() {
+        return null;
+    }
+}
---
+++ alerp-spring-boot/src/main/java/edu/nju/alerp/vo/SummaryProductInfoVO.java
@@ -0,0 +1,25 @@
+package edu.nju.alerp.vo;
+
+/**
+ * 汇总商品信息VO
+ * @author luhailong
+ * @date 2020/01/12
+ */
+public class SummaryProductInfoVO {
+    /**
+     * 商品id
+     */
+    private Integer id;
+    /**
+     * 商品名称
+     */
+    private String name;
+    /**
+     * 商品总重量
+     */
+    private Double totalWeight;
+    /**
+     * 商品平均价格
+     */
+    private Double averagePrice;
+}
