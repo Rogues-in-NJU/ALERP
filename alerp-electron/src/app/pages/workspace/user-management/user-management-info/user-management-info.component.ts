dccf7434160cda5e39b2fbb27fd0340d831a8534
xujiangcheng <mf1832197@smail.nju.edu.cn>
2020-01-15 18:28:23 +0800

    管理员修改用户权限

--- alerp-electron/src/app/pages/workspace/user-management/user-management-info/user-management-info.component.ts
+++ alerp-electron/src/app/pages/workspace/user-management/user-management-info/user-management-info.component.ts
@@ -45,34 +45,51 @@ export class UserManagementInfoComponent implements ClosableTab, OnInit {
         }
         this.isLoading = false;
         this.userManagementData = res.data;
-        // console.log(this.userManagementData)
+        console.log(this.userManagementData)
       }, (error: HttpErrorResponse) => {
         this.message.error(error.message);
       });
   }
 
-  // saveUser(): void {
-  //   if (!this.userManagementForm.valid) {
-  //     return;
-  //   }
-  //   let formData: any = this.userManagementForm.getRawValue();
-  //   let userManagementAdd: UserManagementInfoVO = {
-  //     name: formData.name,
-  //     phoneNumber: formData.phoneNumber,
-  //   };
-  //
-  //   this.isSaving = true;
-  //   this.userManagement.save(userManagementAdd)
-  //     .pipe(debounceTime(3000))
-  //     .subscribe((res: ResultVO<any>) => {
-  //       console.log(res);
-  //       this.message.success('修改成功!');
-  //       this.isSaving = false;
-  //       // TODO: 跳转回列表页面
-  //     }, (error: HttpErrorResponse) => {
-  //       this.message.error(error.message);
-  //     });
-  // }
+  confirmAdd(id: number): void {
+    let userInfo: UserManagementInfoVO = this.userManagementData;
+    for (const tmp of userInfo.authList) {
+      if (tmp.authId == id) {
+        tmp.action = 1;
+      }
+    }
+    console.log(userInfo);
+    this.isSaving = true;
+    this.userManagement.save(userInfo)
+      .pipe(debounceTime(3000))
+      .subscribe((res: ResultVO<any>) => {
+        console.log(res);
+        this.message.success('修改成功!');
+        this.isSaving = false;
+      }, (error: HttpErrorResponse) => {
+        this.message.error(error.message);
+      });
+  }
+
+  confirmAbandon(id: number): void {
+    let userInfo: UserManagementInfoVO = this.userManagementData;
+    for (const tmp of userInfo.authList) {
+      if (tmp.authId == id) {
+        tmp.action = 0;
+      }
+    }
+    console.log(userInfo);
+    this.isSaving = true;
+    this.userManagement.save(userInfo)
+      .pipe(debounceTime(3000))
+      .subscribe((res: ResultVO<any>) => {
+        console.log(res);
+        this.message.success('修改成功!');
+        this.isSaving = false;
+      }, (error: HttpErrorResponse) => {
+        this.message.error(error.message);
+      });
+  }
 
   tabClose(): void {
     this.closeTabService.closeEvent.emit({
