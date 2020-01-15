ce9f687a00e96e23344d008ea4fc7b7750451952
xujiangcheng <mf1832197@smail.nju.edu.cn>
2020-01-15 17:17:07 +0800

    修改个人信息

--- alerp-electron/src/app/core/services/user-management.service.ts
+++ alerp-electron/src/app/core/services/user-management.service.ts
@@ -46,6 +46,7 @@ export class UserManagementService {
   }
 
   public save(info: UserManagementInfoVO): Observable<ResultVO<any>> {
+    console.log(JSON.stringify(info));
     return this.http.post<ResultVO<any>>(`${AppConfig.BASE_URL}/api/user`, info, {
       withCredentials: true
     });
