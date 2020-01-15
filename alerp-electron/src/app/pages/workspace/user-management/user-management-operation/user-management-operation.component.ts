d2a921abeab507c8a3db99b6586d3676fd3c6065
xujiangcheng <mf1832197@smail.nju.edu.cn>
2020-01-15 19:25:17 +0800

    Fix 用户操作记录分页问题

--- alerp-electron/src/app/pages/workspace/user-management/user-management-operation/user-management-operation.component.ts
+++ alerp-electron/src/app/pages/workspace/user-management/user-management-operation/user-management-operation.component.ts
@@ -25,6 +25,8 @@ export class UserManagementOperationComponent implements RefreshableTab, OnInit 
   timeRange: Date[];
 
   operationList: OperationInfoVO[] = [];
+  //更改查询条件时，页数重置为1
+  shouldResetIndex: boolean = false;
 
   constructor(private router: Router,
               private Operation: OperationService,
@@ -38,6 +40,7 @@ export class UserManagementOperationComponent implements RefreshableTab, OnInit 
   }
 
   search(): void {
+
     const queryParams: TableQueryParams = {
       pageIndex: this.pageIndex,
       pageSize: this.pageSize
@@ -46,7 +49,7 @@ export class UserManagementOperationComponent implements RefreshableTab, OnInit 
       Object.assign(queryParams, {
         userName: this.userName
       });
-      this.userName = null;
+      // this.userName = null;
     }
     if (Objects.valid(this.timeRange) && this.timeRange.length === 2) {
       Object.assign(queryParams, {
@@ -75,6 +78,18 @@ export class UserManagementOperationComponent implements RefreshableTab, OnInit 
   }
 
   refresh(): void {
+    this.search();
+  }
+
+  resetQueryParams(): void {
+    this.userName = null;
+    this.timeRange = [];
+    this.shouldResetIndex = false;
+    this.refresh();
+  }
+
+  resetIndex(): void {
+    this.shouldResetIndex = true;
   }
 
 }
