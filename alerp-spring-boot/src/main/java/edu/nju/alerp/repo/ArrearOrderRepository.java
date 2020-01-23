38cdd882295375df79c552a7cbe150954480168d
龙宝 <luhailong.lhl@alibaba-inc.com>
2020-01-24 00:11:23 +0800

    修复bug：欠款统计，按照customerId排序

--- alerp-spring-boot/src/main/java/edu/nju/alerp/repo/ArrearOrderRepository.java
+++ alerp-spring-boot/src/main/java/edu/nju/alerp/repo/ArrearOrderRepository.java
@@ -26,13 +26,16 @@ public interface ArrearOrderRepository extends JpaRepository<ArrearOrder, Intege
     @Query(value =
         "select customer_id as customerId, DATE_FORMAT(due_date, '%Y-%m') as month, sum(receivable_cash - "
             + "received_cash) as cash from "
-            + "arrear_order where (status = 1 or  status = 2) and city=:city group by customer_id, month", nativeQuery = true)
+            + "arrear_order where (status = 1 or  status = 2) and city=:city group by customer_id, month order by "
+            + "customer_id",
+        nativeQuery = true)
     List<Map<String, Object>> getOverdueCashBySbByMonth(@Param("city") int city);
 
     /**
      * 按照月返回欠款金额。
      * 这里的数据结构太过复杂，故意多查一次数据库牺牲性能简化逻辑层的数据结构拼装。
      * 其实只需要上面这个按人按月的统计方法就可以得到所有数据的。
+     *
      * @return
      */
     @Query(value = "select DATE_FORMAT(due_date, '%Y-%m') as month, sum(receivable_cash - received_cash) as cash from"
