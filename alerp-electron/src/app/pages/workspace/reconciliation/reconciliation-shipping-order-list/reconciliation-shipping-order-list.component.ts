8e4c8c972982d2c27479364eec1df6634b7b9d7e
xujiangcheng <mf1832197@smail.nju.edu.cn>
2020-02-09 23:14:30 +0800

    print

--- alerp-electron/src/app/pages/workspace/reconciliation/reconciliation-shipping-order-list/reconciliation-shipping-order-list.component.ts
+++ alerp-electron/src/app/pages/workspace/reconciliation/reconciliation-shipping-order-list/reconciliation-shipping-order-list.component.ts
@@ -52,6 +52,15 @@ export class ReconciliationShippingOrderListComponent implements RefreshableTab,
               private tab: TabService,
               private elRef: ElementRef) {
     this.customerId = this.route.snapshot.params['id'];
+
+    this.printCSS = ['http://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css'];
+
+    this.printStyle =
+      `
+        th, td {
+            color: blue !important;
+        }
+        `;
   }
 
   ngOnInit(): void {
